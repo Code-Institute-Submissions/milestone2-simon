2278f253a5ce9e972e3fccd9c3b55c870882a9cd
Patrick O Neill <patrickonei@gmail.com>
2019-01-17 14:25:42 +0000

    used js to place the elements onto the page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,20 @@
 console.log("HELLO");
+
+const simon = document.getElementById("game");
+console.log(simon);
+const topL = document.createElement("div");
+topL.setAttribute("id", "topleft")
+topL.setAttribute("class", "col-md-12 col-sm-6");
+simon.appendChild(topL);
+const topR = document.createElement("div");
+topR.setAttribute("id", "topRight")
+topR.setAttribute("class", "col-md-12 col-sm-6")
+simon.appendChild(topR);
+const bottomL = document.createElement("div");
+bottomL.setAttribute("id", "bottomLeft")
+bottomL.setAttribute("class", "col-md-12 col-sm-6")
+simon.appendChild(bottomL);
+const bottomR = document.createElement("div");
+bottomR.setAttribute("id", "bottomRight")
+bottomR.setAttribute("class", "col-md-12 col-sm-6")
+simon.appendChild(bottomR);
