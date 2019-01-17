3a722ea154afe3b85400d2907fc3aaffcc93bf2c
Patrick O Neill <patrickonei@gmail.com>
2019-01-17 16:31:32 +0000

    small layout changes and code refactoring in css

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,21 @@
-console.log("HELLO");
-
 const simon = document.getElementById("game");
 console.log(simon);
 const topL = document.createElement("div");
 topL.setAttribute("id", "topleft")
-topL.setAttribute("class", "col-md-12 col-sm-6");
+topL.setAttribute("class", "col-md-12 col-sm-6 col-xs-6");
 simon.appendChild(topL);
+
 const topR = document.createElement("div");
 topR.setAttribute("id", "topRight")
-topR.setAttribute("class", "col-md-12 col-sm-6")
+topR.setAttribute("class", "col-md-12 col-sm-6 col-xs-6")
 simon.appendChild(topR);
+
 const bottomL = document.createElement("div");
 bottomL.setAttribute("id", "bottomLeft")
-bottomL.setAttribute("class", "col-md-12 col-sm-6")
+bottomL.setAttribute("class", "col-md-12 col-sm-6 col-xs-6")
 simon.appendChild(bottomL);
+
 const bottomR = document.createElement("div");
 bottomR.setAttribute("id", "bottomRight")
-bottomR.setAttribute("class", "col-md-12 col-sm-6")
+bottomR.setAttribute("class", "col-md-12 col-sm-6 col-xs-6")
 simon.appendChild(bottomR);
