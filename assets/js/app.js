d1982e023513b7c4631220e6d76a8475057b5ab3
Patrick O Neill <patrickonei@gmail.com>
2019-01-17 19:00:20 +0000

    added labels and innerHTML in the game

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,41 +3,51 @@ const button = document.getElementById("switch");
 console.log(simon);
 console.log(button);
 
-const startGame = document.createElement("button");
-startGame.setAttribute("id", "start");
-startGame.setAttribute("class", "button switches");
-button.appendChild(startGame);
+var label = document.createElement("label");
+var label2 = document.createElement("label");
 
 const power = document.createElement("input");
-power.setAttribute("id", "on");
+power.setAttribute("id", "checkboxSuccess");
 power.setAttribute("type", "checkbox");
-power.setAttribute("class", "toggle left-align switches");
+power.setAttribute("class", "toggle left-align");
+label.setAttribute("placeholder", "Power On");
+label.innerHTML = "Power On"
 button.appendChild(power);
+button.appendChild(label);
 
-const difficulty = document.createElement("button");
-difficulty.setAttribute("id", "diff");
+const startGame = document.createElement("button");
+startGame.setAttribute("id", "start");
+startGame.setAttribute("class", "btn btn-success");
+startGame.innerHTML = "Start Game";
+button.appendChild(startGame);
+
+const difficulty = document.createElement("input");
+difficulty.setAttribute("id", "checkboxError");
 difficulty.setAttribute("type", "checkbox");
-difficulty.setAttribute("class", "toggle right-align switches");
+difficulty.setAttribute("class", "toggle right-align");
+label2.setAttribute("placeholder", "Difficult");
+label2.innerHTML = "Hard Mode"
 button.appendChild(difficulty);
+button.appendChild(label2)
 
 const topL = document.createElement("div");
-topL.setAttribute("id", "topleft")
-topL.setAttribute("class", "col-md-12 col-sm-6 col-xs-6");
+topL.setAttribute("id", "topleft");
+topL.setAttribute("class", "col-md-6 col-sm-6 col-xs-6");
 simon.appendChild(topL);
 
 const topR = document.createElement("div");
-topR.setAttribute("id", "topRight")
-topR.setAttribute("class", "col-md-12 col-sm-6 col-xs-6")
+topR.setAttribute("id", "topRight");
+topR.setAttribute("class", "col-md-6 col-sm-6 col-xs-6");
 simon.appendChild(topR);
 
 const bottomL = document.createElement("div");
-bottomL.setAttribute("id", "bottomLeft")
-bottomL.setAttribute("class", "col-md-12 col-sm-6 col-xs-6")
+bottomL.setAttribute("id", "bottomLeft");
+bottomL.setAttribute("class", "col-md-6 col-sm-6 col-xs-6");
 simon.appendChild(bottomL);
 
 const bottomR = document.createElement("div");
-bottomR.setAttribute("id", "bottomRight")
-bottomR.setAttribute("class", "col-md-12 col-sm-6 col-xs-6")
+bottomR.setAttribute("id", "bottomRight");
+bottomR.setAttribute("class", "col-md-6 col-sm-6 col-xs-6");
 simon.appendChild(bottomR);
 
 const counter = document.createElement("div");
@@ -46,4 +56,7 @@ simon.appendChild(counter);
 
 const countScreen = document.createElement("div");
 countScreen.setAttribute("class", "game-count");
+countScreen.innerHTML = "---";
 simon.appendChild(countScreen);
+
+
