f123c24a0800a881c2c2c8023ebaf4053ef22723
Patrick O Neill <patrickonei@gmail.com>
2019-01-28 12:17:26 +0000

    uncommented code

--- assets/spec/appSpec.js
+++ assets/spec/appSpec.js
@@ -1,34 +1,34 @@
-// describe("Testing power", () => {
-//     describe("Power Button", () => {
-//         it("should return false", () => {
-//             expect(power.checked).toBe(false);
-//         });
-//     });
-// });
+describe("Testing power", () => {
+    describe("Power Button", () => {
+        it("should return false", () => {
+            expect(power.checked).toBe(false);
+        });
+    });
+});
 
-// describe("Testing difficult", () => {
-//     describe("Diff Button", () => {
-//         it("should return false", () => {
-//             expect(difficulty.checked).toBe(false);
-//         });
-//     });
-// });
+describe("Testing difficult", () => {
+    describe("Diff Button", () => {
+        it("should return false", () => {
+            expect(difficulty.checked).toBe(false);
+        });
+    });
+});
 
-// describe("Check Readout", () => {
-//     describe("counter screen", () => {
-//         it("should return '---'", () => {
-//             expect(countScreen.innerHTML).toBe("---");
-//         });
-//     });
-// });
+describe("Check Readout", () => {
+    describe("counter screen", () => {
+        it("should return '---'", () => {
+            expect(countScreen.innerHTML).toBe("---");
+        });
+    });
+});
 
-// describe("Check Readout", () => {
-//     describe("prompt screen", () => {
-//         it("should return 'power me on'", () => {
-//             expect(counter.innerHTML).toBe("POWER ME ON!");
-//         });
-//     });
-// });
+describe("Check Readout", () => {
+    describe("prompt screen", () => {
+        it("should return 'power me on'", () => {
+            expect(counter.innerHTML).toBe("POWER ME ON!");
+        });
+    });
+});
 
 describe("checking arrays", () => {
     it("should be empty for player", () => {
